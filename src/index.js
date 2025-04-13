64236a8f1829c9f38161ec62bd01f8f20507217b
Kristijan Turić <kristijan.turic1@gmail.com>
2025-04-13 14:51:55 +0200

    Add loading spinner, fix unit switching

--- src/index.js
+++ src/index.js
@@ -13,33 +13,49 @@ let isMetric = !unitToggle.checked;
 // City Search and Input
 const cityInput = document.getElementById("citySearch");
 const cityInputSearchIcon = document.getElementById("citySearchIcon");
+const weatherCard = document.getElementById("weatherCard");
+let lastCity = "";
 
 cityInput.addEventListener("keydown", async(event) => {
     if (event.key === "Enter") {
-        await searchCityWeather(cityInput);
+        lastCity = cityInput.value;
+        await searchCityWeather(cityInput.value);
     }
 })
 
 cityInputSearchIcon.addEventListener("click", async() => {
-    await searchCityWeather(cityInput);
+    lastCity.value = cityInput;
+    await searchCityWeather(cityInput.value);
 });
 
 // Gets data, displays it and clears the city input
 async function searchCityWeather(city) {
-    let currentWeather = await getCurrentWeatherData(city.value, isMetric);
-    displayWeather(currentWeather, isMetric);
+    const spinner = document.getElementById("loading-spinner");
+    spinner.style.display = "block";
+    weatherCard.style.display = "none";
+
+    try {
+        let currentWeather = await getCurrentWeatherData(city, isMetric);
+        displayWeather(currentWeather, isMetric);
+    } catch(error) {
+        console.error("Failed to fetch data: ", error);
+    } finally {
+        spinner.style.display = "none";
+        weatherCard.style.display = "flex";
+        cityInput.value = "";
+    }
 }
 
 // Unit toggle
-unitToggle.addEventListener("change", () => {
+unitToggle.addEventListener("change", async() => {
     // It is set to Celsius
     if (!unitToggle.checked) {
         isMetric = true;
-        cityInputSearchIcon.click();
+        await searchCityWeather(lastCity);
     }
     // It is set to Fahrenheit
     else {
         isMetric = false;
-        cityInputSearchIcon.click();
+        await searchCityWeather(lastCity);
     }
 });
